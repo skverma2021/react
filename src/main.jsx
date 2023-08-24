407146435ea2e95c879d7052a4f6a594d62e19e1
SK Verma <s.k.verma@live.in>
2023-08-25 05:02:29 +0530

    TPcontext added CRUD empDesig and empDeptt done

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import UserState from './context/appUser/UserState.jsx';
 import './index.css';
+import TPState from './context/tp/TPState';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserState>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <TPState>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </TPState>
     </UserState>
   </React.StrictMode>
 );
