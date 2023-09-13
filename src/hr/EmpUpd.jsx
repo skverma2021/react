1076a2b5890fe26b69376dde613aab576569e192
SK Verma <s.k.verma@live.in>
2023-09-13 16:43:41 +0530

    empUpd-html validation added

--- src/hr/EmpUpd.jsx
+++ src/hr/EmpUpd.jsx
@@ -45,7 +45,8 @@ const EmpUpd = () => {
     setEmp({ ...emp, [e.target.name]: e.target.value });
   };
 
-  const updEmpData = async () => {
+  const updEmpData = async (event) => {
+    event.preventDefault();
     try {
       await axios.put(`http://localhost:3000/api/emps/${id}`, {
         ...emp,
@@ -56,6 +57,11 @@ const EmpUpd = () => {
       setErr(error.message);
     }
   };
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   alert(`The name you entered was: ${name}`)
+  // }
   if (err) return <div>Error: {err}</div>;
   return (
     <div
@@ -69,219 +75,246 @@ const EmpUpd = () => {
         width: '20%',
       }}
     >
-      <h4>Edit an Employee</h4>
+      <h2>Edit an Employee</h2>
       <br />
       <br />
-
-      <label>
-        <strong>Unique ID</strong>
-      </label>
-      <br />
-      <input
-        name='uId'
-        value={emp.uId || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <label>
-        <strong>First Name:</strong>
-      </label>
-      <br />
-      <input
-        name='fName'
-        value={emp.fName || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <label>
-        <strong>Middle Name:</strong>
-      </label>
-      <br />
-      <input
-        name='mName'
-        value={emp.mName || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <label>
-        <strong>Surname:</strong>
-      </label>
-      <br />
-      <input
-        name='sName'
-        value={emp.sName || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <label>
-        <strong>Title:</strong>
-      </label>
-      <br />
-      <div style={{ display: 'flex' }}>
-        <input
-          type='radio'
-          id='Mr'
-          name='title'
-          value='Mr'
-          checked={emp.title == 'Mr'}
-          onChange={(e) => {
-            return onValChange(e);
-          }}
-        />
-          <label for='Mr'>Mr</label>
-      </div>
-      <div style={{ display: 'flex' }}>
-        <input
-          type='radio'
-          id='Ms'
-          name='title'
-          value='Ms'
-          checked={emp.title == 'Ms'}
-          onChange={(e) => {
-            return onValChange(e);
-          }}
-        />
-          <label for='Ms'>Ms</label>
-      </div>
-      <br />
-
-      <label>
-        <strong>Date of Birth:</strong>
+      <form onSubmit={updEmpData}>
+        <table style={{ lineHeight: '35px' }}>
+          <tbody>
+            <tr>
+              <td>
+                <strong>Unique ID</strong>
+              </td>
+              <td>
+                <input
+                  name='uId'
+                  value={emp.uId || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>First Name:</strong>
+              </td>
+              <td>
+                <input
+                  name='fName'
+                  minLength={3}
+                  value={emp.fName || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Middle Name:</strong>
+              </td>
+              <td>
+                <input
+                  name='mName'
+                  value={emp.mName || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Surname:</strong>
+              </td>
+              <td>
+                <input
+                  name='sName'
+                  value={emp.sName || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Title:</strong>
+              </td>
+              <td>
+                <div style={{ display: 'flex' }}>
+                  <input
+                    type='radio'
+                    id='Mr'
+                    name='title'
+                    value='Mr'
+                    checked={emp.title == 'Mr'}
+                    onChange={(e) => {
+                      return onValChange(e);
+                    }}
+                  />
+                    <label for='Mr'>Mr</label>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <input
+                    type='radio'
+                    id='Ms'
+                    name='title'
+                    value='Ms'
+                    checked={emp.title == 'Ms'}
+                    onChange={(e) => {
+                      return onValChange(e);
+                    }}
+                  />
+                    <label for='Ms'>Ms</label>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Date of Birth:</strong>
+              </td>
+              <td>
+                <input
+                  name='dob'
+                  type='date'
+                  min='1960-01-01'
+                  max='2004-12-31'
+                  // value={emp.dob || ''}
+                  value={moment(emp.dob).format('YYYY-MM-DD')}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Gender:</strong>
+              </td>
+              <td>
+                <div style={{ display: 'flex' }}>
+                  <input
+                    type='radio'
+                    id='male'
+                    name='gender'
+                    value='M'
+                    checked={emp.gender == 'M'}
+                    onChange={(e) => {
+                      return onValChange(e);
+                    }}
+                  />
+                    <label for='male'>Male</label>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <input
+                    type='radio'
+                    id='female'
+                    name='gender'
+                    value='F'
+                    checked={emp.gender == 'F'}
+                    onChange={(e) => {
+                      return onValChange(e);
+                    }}
+                  />
+                    <label for='female'>Female</label>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Address:</strong>
+              </td>
+              <td>
+                <input
+                  name='addLine1'
+                  value={emp.addLine1 || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>City:</strong>
+              </td>
+              <td>
+                {' '}
+                <select
+                  name='cityId'
+                  id='cityId'
+                  value={theCity || ''}
+                  onChange={(e) => {
+                    return setTheCity(e.target.value);
+                  }}
+                >
+                  {cities.map((c) => {
+                    return (
+                      <option key={c.id} value={c.id}>
+                        {c.cityName}
+                      </option>
+                    );
+                  })}
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Mobile:</strong>
+              </td>
+              <td>
+                <input
+                  name='mobile'
+                  type='number'
+                  min='1000000000'
+                  max='9999999999'
+                  value={emp.mobile || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>eMail ID:</strong>
+              </td>
+              <td>
+                <input
+                  name='eMailId'
+                  type='email'
+                  value={emp.eMailId || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Password:</strong>
+              </td>
+              <td>
+                <input
+                  name='passwd'
+                  type='password'
+                  value={emp.passwd || ''}
+                  onChange={(e) => {
+                    return onValChange(e);
+                  }}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
         <br />
-      </label>
-      <input
-        name='dob'
-        type='date'
-        // value={emp.dob || ''}
-        value={moment(emp.dob).format('YYYY-MM-DD')}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
 
-      <label>
-        <strong>Gender:</strong>
-      </label>
-      <br />
-      <div style={{ display: 'flex' }}>
-        <input
-          type='radio'
-          id='male'
-          name='gender'
-          value='M'
-          checked={emp.gender == 'M'}
-          onChange={(e) => {
-            return onValChange(e);
-          }}
-        />
-          <label for='male'>Male</label>
-      </div>
-      <div style={{ display: 'flex' }}>
-        <input
-          type='radio'
-          id='female'
-          name='gender'
-          value='F'
-          checked={emp.gender == 'F'}
-          onChange={(e) => {
-            return onValChange(e);
-          }}
-        />
-          <label for='female'>Female</label>
-      </div>
-      <br />
+        <input type='submit' />
+      </form>
 
-      <label>
-        <strong>Address:</strong>
-      </label>
-      <br />
-      <input
-        name='addLine1'
-        value={emp.addLine1 || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <label>
-        <strong>City:</strong>
-      </label>
-      <br />
-      <select
-        name='cityId'
-        id='cityId'
-        value={theCity || ''}
-        onChange={(e) => {
-          return setTheCity(e.target.value);
-        }}
-      >
-        {cities.map((c) => {
-          return (
-            <option key={c.id} value={c.id}>
-              {c.cityName}
-            </option>
-          );
-        })}
-      </select>
-      <br />
-
-      <label>
-        <strong>Mobile:</strong>
-      </label>
-      <br />
-      <input
-        name='mobile'
-        type='tel'
-        value={emp.mobile || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <label>
-        <strong>eMail ID:</strong>
-      </label>
-      <br />
-      <input
-        name='eMailId'
-        type='email'
-        value={emp.eMailId || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <label>
-        <strong>Password:</strong>
-      </label>
-      <br />
-      <input
-        name='passwd'
-        type='password'
-        value={emp.passwd || ''}
-        onChange={(e) => {
-          return onValChange(e);
-        }}
-      />
-      <br />
-
-      <button onClick={updEmpData}>Update</button>
+      {/* <button onClick={updEmpData}>Update</button> */}
     </div>
   );
 };
